79f14b819253d75cd5717d1e3164b91f0081cbf2
Nadir <nadir.berrabia@ugent.be>
2021-02-26 15:43:39 +0100

    basic skeleton for HTML loading and testing

--- __init__.py
+++ __init__.py
@@ -1,4 +1,16 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)  # de flask app
+app.config['SECRET_KEY'] = 'fd3d2cbd4d8fafa22eafa6bf32b26797'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# dit is een beveiligingskey waar ik de werking beetje vn ben vergeten maar bassicly da runt de pagina ni
+# als de gebruikers key ni de secretkey matcht, die key heb ik via python in terminal via een module gecreert
+# kweni zeker ofda ook hash is fni though
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)  # gebruik ik om ww te hashen en te decrypte om te checken
+login_manager = LoginManager(app)  # daddy's fav klasse of all time baby, zo vanzelfsprekend jeeezus i love it
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/index")
+def index():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
     app.run()
+
